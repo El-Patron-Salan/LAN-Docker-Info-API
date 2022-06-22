7cf2a87f8b89d2c95bc2a0e6bdda3c3f86256ab5
El-Patron-Salan <ca09062000@gmail.com>
2022-06-22 12:52:44 +0200

    Add more API methods

--- LANDockerInfoAPI/Controllers/DockerInfoController.cs
+++ LANDockerInfoAPI/Controllers/DockerInfoController.cs
@@ -10,19 +10,35 @@ namespace LANDockerInfoAPI.Controllers
         [HttpGet("SystemInformation")]
         public List<SystemProperties> GetSystemInformation()
         {
-            return SerializationController<SystemProperties>.Deserialize("SystemInformation.json");
+            return SerializationController<List<SystemProperties>>.Deserialize("SystemInformation.json");
         }
+
 
         [HttpGet("ContainersInformation")]
         public List<ContainerProperties> GetContainersInformation()
         {
-            return SerializationController<ContainerProperties>.Deserialize("ContainersInformation.json");
+            return SerializationController<List<ContainerProperties>>.Deserialize("ContainersInformation.json");
         }
+
+        [HttpGet("ContainersInformation/{image}")]
+        public ContainerProperties GetContainerByImage([FromRoute] string image)
+        {
+            var deserialized = SerializationController<List<ContainerProperties>>.Deserialize("ContainersInformation.json");
+            return deserialized.FirstOrDefault(_ => _.Image == image) ?? throw new Exception("Container not found");
+        }
+
 
         [HttpGet("ImagesInformation")]
         public List<ImageProperties> GetImagesInformation()
         {
-            return SerializationController<ImageProperties>.Deserialize("ImagesInformation.json");
+            return SerializationController<List<ImageProperties>>.Deserialize("ImagesInformation.json");
+        }
+
+        [HttpGet("ImagesInformation/{repo}")]
+        public ImageProperties GetImageByRepo([FromRoute] string repo)
+        {
+            var deserialized = SerializationController<List<ImageProperties>>.Deserialize("ImagesInformation.json");
+            return deserialized.FirstOrDefault(_ => _.Repository == repo) ?? throw new Exception("Image not found");
         }
     }
 }
--- LANDockerInfoAPI/Controllers/SerializationController.cs
+++ LANDockerInfoAPI/Controllers/SerializationController.cs
@@ -5,14 +5,14 @@ namespace LANDockerInfoAPI.Controllers
     internal class SerializationController<T>
     {
         ///Serialization is obsolete for now - it's a readonly API
-        internal static List<T> Deserialize(string path)
+        internal static T Deserialize(string path)
         {
             String json;
             using (StreamReader streamReader = new(@$"S:\{path}", encoding: System.Text.Encoding.UTF8))
             {
                 json = streamReader.ReadToEnd();
             }
-            List<T> obj = JsonConvert.DeserializeObject<List<T>>(json) ?? throw new Exception("SerializationController.sc: deserialization failed line 20");
+            T obj = JsonConvert.DeserializeObject<T>(json) ?? throw new Exception("SerializationController.sc: deserialization failed line 20");
             return obj;
         }
     }
